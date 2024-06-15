d5fc00e8a1deeef36936dc521b2643cf0406aadc
Matilda <ogarimatilda@gmail.com>
2024-06-15 17:25:36 +0300

    second commit

---
+++ src/Components/SideNavbar.jsx
@@ -0,0 +1,51 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { NavLink } from 'react-router-dom';
+
+function SideNavbar() {
+  return (
+    <>
+      {[false].map((expand) => (
+        <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
+          <Container fluid>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="start"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Offcanvas
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                <NavLink to="/Maps" className="nav-link">My Map</NavLink>
+                <NavLink to="/Jotting" className="nav-link">Plans</NavLink>
+                <NavLink to="/DailyLogs" className="nav-link">Travel Journal</NavLink>
+                </Nav>
+                <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                  />
+                  <Button variant="outline-success">Search</Button>
+                </Form>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+            <Navbar.Brand  as={NavLink} to="/" className="me-auto">VentureLog</Navbar.Brand>
+          </Container>
+        </Navbar>
+      ))}
+    </>
+  );
+}
+
+export default SideNavbar;
